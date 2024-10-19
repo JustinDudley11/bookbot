16f9f938d1eb239816dd96fb84e5d3cb5b427f2a
JustinDudley11 <justindudley11@gmail.com>
2024-10-19 00:41:00 -0400

    updated count_character with sort and filter and finished report

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ def main():
     text = get_book_text(book_path)
     num_words = count_word(text)
     num_character = count_character(text)
-    print(f"{num_words} words found in the document")
-    print(num_character)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document.")
+    for let in num_character:
+        print(f"The {let} character was found {num_character[let]} times")
+    print("--- End Report ---")
     
 def count_word(file):
     words = file.split()
@@ -18,11 +21,12 @@ def count_character(file):
     chars_dict = {}
     for c in file:
         lowered = c.lower()
-        if lowered in chars_dict:
-            chars_dict[lowered] += 1
-        else:
-            chars_dict[lowered] = 1
-    return chars_dict
+        if lowered.isalpha():
+            if lowered in chars_dict:
+                chars_dict[lowered] += 1
+            else:
+                chars_dict[lowered] = 1
+    return dict(sorted(chars_dict.items(), key=lambda item: item[1], reverse=True))
 
 main()
 
